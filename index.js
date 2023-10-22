2476c56ccb6233f9208b71d5d22a39c4e8d082c0
Tobexxy <tobiogunleye76@gmail.com>
2023-10-22 21:23:55 +0100

    22.10.23

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
-document.querySelectorAll("button")[3].addEventListener("click", handleClick);
+//document.querySelectorAll("button")[3].addEventListener("click", handleClick);
 
-var buttonnuber = document.querySelectorAll("button").length;
+
 
 // you can use the event listener function in to different ways the top or the button one
 //document.querySelectorAll("button").addEventListener("click", function () { 
 //         alert("i got clicked by the mouse")} );
 
+
+var buttonnuber = document.querySelectorAll("button").length;
 for (var i = 0; i<buttonnuber; i++){
     document.querySelectorAll("button")[i].addEventListener("click", handleClick);
 
 }
 
 function handleClick() {
-    alert("i got clicked");
+    var buttonInnerHTML = this.innerHTML
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var audio = new Audio("/sounds/crash.mp3");
+            audio.play();
+        break;
+        
+        case "a":
+            var audio1 = new Audio("/sounds/kick-bass.mp3");
+            audio1.play();
+        break;
+
+        case "s":
+            var audio2 = new Audio("/sounds/snare.mp3");
+            audio2.play();
+        break;
+
+        case "d":
+            var audio3 = new Audio("/sounds/tom-1.mp3");
+            audio3.play();
+        break;
+
+        case "j":
+            var audio4 = new Audio("/sounds/tom-2.mp3");
+            audio4.play();
+        break;
+
+        case "k":
+            var audio5 = new Audio("/sounds/tom-3.mp3");
+            audio5.play();
+        break;
+
+        case "l":
+            var audio6 = new Audio("/sounds/tom-4.mp3");
+            audio6.play();
+        break;
+
+        default:
+            console.log();
+    
+    }
+    
+    
 }
 
+//audio.play();
